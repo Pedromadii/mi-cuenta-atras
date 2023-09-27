c6c349e409785e2eb4ea615a2bdccb7d8e1f4f1c
Pedromadii <96880690+Pedromadii@users.noreply.github.com>
2023-09-27 11:35:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const targetDate = new Date("2024-01-01T00:00:00"); // Configura la fecha de destino aquí
+    const currentDate = new Date();
+    const totalSeconds = (targetDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    document.getElementById('days').innerHTML = days + " días";
+    document.getElementById('hours').innerHTML = hours + " horas";
+    document.getElementById('minutes').innerHTML = minutes + " minutos";
+    document.getElementById('seconds').innerHTML = seconds + " segundos";
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
